7cf03e98db81a42cc41ff6efc8b52e72dd90af66
Veronica Bustabad <vbustabad@gmail.com>
2018-09-24 20:31:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,35 +17,35 @@ function forkRepo() {
   .then(json => showResults(json))
 }
 
-function showResults(json) {
-  const forkedLab = `<a href=${json.html_url}>Forked Lab</a>`;
-  document.getElementById('results').innerHTML = forkedLab;
-}
-
-function createIssue() {
-  const postData = {
-    title: document.getElementById("title").value,
-    body: document.getElementById("body").value
-    }
-    fetch(
-      `https://api.github.com/repos/vbustabad/${repo}/issues`,
-      {
-      method: 'POST',
-      headers: {
-        Authorization: `token ${getToken()}`
-      },
-      body: JSON.stringify(postData)
-    }).then(resp => resp.json())
-    .then(json => getIssues())
-  }
-}
-
-function getIssues() {
-  fetch(`https://api.github.com/repos/vbustabad/js-ajax-fetch-lab/issues`, {
-    headers: {
-      Authorization: `token ${getToken()}`
-    }
-    })
-    .then(issues => issues.json())
-    .then(json => console.log(json))
-}
+// function showResults(json) {
+//   const forkedLab = `<a href=${json.html_url}>Forked Lab</a>`;
+//   document.getElementById('results').innerHTML = forkedLab;
+// }
+// 
+// function createIssue() {
+//   const postData = {
+//     title: document.getElementById("title").value,
+//     body: document.getElementById("body").value
+//     }
+//     fetch(
+//       `https://api.github.com/repos/vbustabad/${repo}/issues`,
+//       {
+//       method: 'POST',
+//       headers: {
+//         Authorization: `token ${getToken()}`
+//       },
+//       body: JSON.stringify(postData)
+//     }).then(resp => resp.json())
+//     .then(json => getIssues())
+//   }
+// }
+// 
+// function getIssues() {
+//   fetch(`https://api.github.com/repos/vbustabad/js-ajax-fetch-lab/issues`, {
+//     headers: {
+//       Authorization: `token ${getToken()}`
+//     }
+//     })
+//     .then(issues => issues.json())
+//     .then(json => console.log(json))
+// }
