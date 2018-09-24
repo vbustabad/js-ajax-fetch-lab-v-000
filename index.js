4b97a2ce38d8a30e9b2347f16891ec0f57ba232a
Veronica Bustabad <vbustabad@gmail.com>
2018-09-24 20:34:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,24 +20,24 @@ function showResults(json) {
   const forkedLab = `<a href=${json.html_url}>Forked Lab</a>`;
   document.getElementById('results').innerHTML = forkedLab;
 }
-//
-// function createIssue() {
-//   const postData = {
-//     title: document.getElementById("title").value,
-//     body: document.getElementById("body").value
-//     }
-//     fetch(
-//       `https://api.github.com/repos/vbustabad/${repo}/issues`,
-//       {
-//       method: 'POST',
-//       headers: {
-//         Authorization: `token ${getToken()}`
-//       },
-//       body: JSON.stringify(postData)
-//     }).then(resp => resp.json())
-//     .then(json => getIssues())
-//   }
-// }
+
+function createIssue() {
+  const postData = {
+    title: document.getElementById("title").value,
+    body: document.getElementById("body").value
+    }
+    fetch(
+      `https://api.github.com/repos/vbustabad/${repo}/issues`,
+      {
+      method: 'POST',
+      headers: {
+        Authorization: `token ${getToken()}`
+      },
+      body: JSON.stringify(postData)
+    }).then(resp => resp.json())
+    .then(json => getIssues())
+  }
+}
 //
 // function getIssues() {
 //   fetch(`https://api.github.com/repos/vbustabad/js-ajax-fetch-lab/issues`, {
